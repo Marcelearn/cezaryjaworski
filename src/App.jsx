932644238fb2cd6b2f895aaffcb567639c778e2a
Marcelearn <jaworskimarcel@gmail.com>
2023-06-26 23:41:24 +0200

    transformed applied to vite

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+
+
+function App() {
+  
+
+  return (
+   
+    <main>
+        <div class="firstImg">
+          <img class="horizontal"src="./imagenes/HombresDeLaLuz.JPG" alt="light photography"/>
+        </div>
+        <div class="text">
+          <h1>Cezary Jaworski</h1>
+        </div>
+        <div class="bio">
+          <img class="horizontal"src="https://yt3.ggpht.com/ytc/AKedOLTCQy2XExYclDBJatoHrjBSMPcA0fYWdfW7QVaoGw=s900-c-k-c0x00ffffff-no-rj" alt=""/>
+        </div>
+        <div>
+          <iframe width="720" height="480" src="https://www.youtube.com/embed/T77gtlw1KuE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div>
+          <iframe width="720" height="480" src="https://www.youtube.com/embed/XnG2MkSCByg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div>
+          <iframe width="720" height="480" src="https://www.youtube.com/embed/t3lcaSp1xYI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <section id="gallery-container">
+
+        </section>
+        <div class = "iconContainner">
+          <i class="fa-brands fa-imdb"></i>
+          <i class="fa-brands fa-twitter"></i>
+        </div>
+    </main>
+      
+  
+  )
+}
+
+export default App
