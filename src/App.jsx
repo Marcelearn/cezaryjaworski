972b9d5d5ff65f9a2dccd0c466fb4a8e292a82b3
Marcelearn <jaworskimarcel@gmail.com>
2023-06-27 10:43:45 +0200

    iframe responsive

--- src/App.jsx
+++ src/App.jsx
@@ -30,13 +30,13 @@ function App() {
         <div class="bio">
           <img src="https://yt3.ggpht.com/ytc/AKedOLTCQy2XExYclDBJatoHrjBSMPcA0fYWdfW7QVaoGw=s900-c-k-c0x00ffffff-no-rj" alt=""/>
         </div>
-        <div>
+        <div class="iframe-container">
           <iframe width="720" height="480" src="https://www.youtube.com/embed/T77gtlw1KuE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
-        <div>
+        <div class="iframe-container">
           <iframe width="720" height="480" src="https://www.youtube.com/embed/XnG2MkSCByg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
-        <div>
+        <div class="iframe-container">
           <iframe width="720" height="480" src="https://www.youtube.com/embed/t3lcaSp1xYI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
         <section id="gallery-container">
